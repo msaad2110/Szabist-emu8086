15ccfaa752ad5ba828e0eaec7aa8e82fa84a36a9
fizzaalii <73457636+fizzaalii@users.noreply.github.com>
2022-01-16 20:51:06 +0500

    Update Q11.asm

--- Q11.asm
+++ Q11.asm
@@ -1,21 +1,50 @@
 .model small
-.stack 100h 
-.data 
-.code  
+.stack 100h
+.data
 
-main proc 
-                                               
-mov ah, 1
-int 21h
+msg db ? 
 
-mov dl,al
-inc dl
-          
-mov ah,2 
-int 21h
+ 
+.code
 
-mov ah, 4ch
-int 21h
-
-main endp   
+main proc
+    
+   mov ah,1
+   int 21h
+   mov dl,al
+   
+   mov ah,1
+   int 21h
+   mov bl,al
+   
+   mov ah,1
+   int 21h
+   mov cl,al
+   
+   add dl,bl
+   sub dl,48
+   mov ch,dl
+   add ch,cl
+   sub ch,48
+   
+   mov msg,ch
+   
+   mov dl,10
+   mov ah,2
+   int 21h
+   
+   mov dl,13 
+   mov ah,2
+   int 21h
+   
+   mov ah,2 
+   mov dl,msg
+   int 21h
+    
+   exit:
+   mov ah,4ch
+   int 21h
+   
+   
+   main endp
 end main
